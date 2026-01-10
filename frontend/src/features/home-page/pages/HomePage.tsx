c879d45b10ff6ab8b68fb102a3d9de6892071ad6
Isaque Precioso <caracktergames@gmail.com>
2026-01-10 17:28:37 -0300

    WIP: HomePage style structure

--- frontend/src/features/home-page/pages/HomePage.tsx
+++ frontend/src/features/home-page/pages/HomePage.tsx
@@ -1,12 +1,32 @@
 import { BasePage } from "@/components/layout/BasePage";
-
+import HomeCard from "../components/HomeCard";
+import { Hourglass, UsersRound, Wallet } from "lucide-react";
+import ClientsCardContent from "../components/ClientsCardContent";
+import WalletsCardContent from "../components/WalletsCardContent";
+import DueDatesCardContent from "../components/DueDatesCardContent";
 
 export function HomePage() {
     return (
         <BasePage>
-            {/* h-full agora faz ela ter 100% do espaço que sobrou do header */}
-            <div className="h-full text-white flex flex-col items-center justify-center p-4">
-                <h1>Seu Conteúdo Centralizado</h1>
+            <div className="h-full text-white flex flex-col p-4">
+                <div className="text-center mt-14">
+                    <p className="text-3xl font-bold bg-gradient-to-r from-blue-400 to-emerald-400 bg-clip-text text-transparent">
+                        Olá USUÁRIO, bem-vindo de volta!
+                    </p>
+                    <p className="text-3xl font-bold bg-gradient-to-r from-blue-400 to-emerald-400 bg-clip-text text-transparent mt-4">
+                        {new Date().toLocaleDateString('pt-BR', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })}
+                    </p>
+                </div>
+                <div className=' grid md:grid-cols-3'>
+                    <HomeCard title="Clientes" icon={<UsersRound size={38} />} content={<ClientsCardContent />} />
+                    <HomeCard title="Carteiras" icon={<Wallet size={38} />} content={<WalletsCardContent />} />
+                    <HomeCard title="Vencimentos" icon={<Hourglass size={38} />}content={<DueDatesCardContent />} />
+                </div>
+                <div className='grid md:grid-cols-3'>
+                    <HomeCard title="Card 1" content={<p>Conteúdo do Card 1</p>} />
+                    <HomeCard title="Card 2" content={<p>Conteúdo do Card 2</p>} />
+                    <HomeCard title="Card 3" content={<p>Conteúdo do Card 3</p>} />
+                </div>
             </div>
         </BasePage>
     );
