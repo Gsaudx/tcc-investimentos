d402ce529e7061982c980422cfc38c3b40f5f321
Isaque Precioso <caracktergames@gmail.com>
2026-01-10 15:32:54 -0300

    fix: now HomePage fullfield the page not scrolling

--- frontend/src/features/home-page/pages/HomePage.tsx
+++ frontend/src/features/home-page/pages/HomePage.tsx
@@ -4,7 +4,10 @@ import { BasePage } from "@/components/layout/BasePage";
 export function HomePage() {
     return (
         <BasePage>
-            <div className="min-h-screen text-gray-900 flex flex-col items-center justify-center p-4">caguei duro</div>
+            {/* h-full agora faz ela ter 100% do espaço que sobrou do header */}
+            <div className="h-full text-white flex flex-col items-center justify-center p-4">
+                <h1>Seu Conteúdo Centralizado</h1>
+            </div>
         </BasePage>
     );
 }
